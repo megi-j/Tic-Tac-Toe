13058e1366ccfc01cb8107f4b62f33f30eb4cbd6
gitbush-test <82594147+megi-j@users.noreply.github.com>
2022-11-15 17:54:37 +0400

    added some functions

--- newGameVsPlayer.js
+++ newGameVsPlayer.js
@@ -12,12 +12,17 @@ let winnerO = document.querySelector(".win-O");
 let winnerPlayer = document.querySelector(".who-takes-round span")
 let p2Score =document.querySelector(".p2-score")
 let p1Score = document.querySelector(".p1-score")
+let ties = document.querySelector(".who-takes-round h1")
+let winner = document.querySelector(".winner");
+let winnerInfo =document.querySelector(".who-takes-round p");
+let takesTheRound = document.querySelector(".winner h2");
+let tiesScore = document.querySelector(".ties")
 //აქ ჩამოვივლი ყველა ყუთზე და რომელსაც დავაკლიკებ იმაზე ჩნდება x ნიშანი,ხოლო მეორე ნიშანი რომელიც მაუსის მიტანაზე ჩნდება, კლიკზე ქრება.
 let clicked = false;
 let clickCounter = 1;
 let p2WinScore = 0;
 let p1WinScore = 0;
-
+let tiesWinScore = 0;
 gameBoxes.forEach(box=>{
     clickBox(box)
     hoverBox(box)
@@ -26,40 +31,70 @@ gameBoxes.forEach(box=>{
 
 function clickBox(box){
     box.addEventListener("click", function(){
+        console.log(clickCounter)
         box.lastElementChild.style.display = "none"
-        if(!box.clicked && clickCounter % 2 != 0){
+        if(!box.clicked && clickCounter % 2 !== 0){
             box.firstElementChild.style.display = "block";
             Oturn.style.display = "block";
-            Xturn.style.display = "none"
-            clickCounter++;
+            Xturn.style.display = "none";
             box.lastElementChild.style.display = "none";
+            clickCounter++;
             if((gameBoxes[0].querySelector(".full-X").style.display == "block" && gameBoxes[1].querySelector(".full-X").style.display == "block" && gameBoxes[2].querySelector(".full-X").style.display == "block") || (gameBoxes[0].querySelector(".full-X").style.display == "block" && gameBoxes[3].querySelector(".full-X").style.display == "block" && gameBoxes[6].querySelector(".full-X").style.display == "block") ||
             (gameBoxes[0].querySelector(".full-X").style.display == "block" && gameBoxes[4].querySelector(".full-X").style.display == "block" && gameBoxes[8].querySelector(".full-X").style.display == "block") ||
-            (gameBoxes[1].querySelector(".full-X").style.display == "block" && gameBoxes[4].querySelector(".full-X").style.display == "block" && gameBoxes[7].querySelector(".full-X").style.display == "block")){
+            (gameBoxes[1].querySelector(".full-X").style.display == "block" && gameBoxes[4].querySelector(".full-X").style.display == "block" && gameBoxes[7].querySelector(".full-X").style.display == "block") ||
+            (gameBoxes[2].querySelector(".full-X").style.display == "block" && gameBoxes[4].querySelector(".full-X").style.display == "block" && gameBoxes[6].querySelector(".full-X").style.display == "block") ||
+            (gameBoxes[2].querySelector(".full-X").style.display == "block" && gameBoxes[5].querySelector(".full-X").style.display == "block" && gameBoxes[8].querySelector(".full-X").style.display == "block") ||
+            (gameBoxes[3].querySelector(".full-X").style.display == "block" && gameBoxes[4].querySelector(".full-X").style.display == "block" && gameBoxes[5].querySelector(".full-X").style.display == "block") ||
+            (gameBoxes[6].querySelector(".full-X").style.display == "block" && gameBoxes[7].querySelector(".full-X").style.display == "block" && gameBoxes[8].querySelector(".full-X").style.display == "block")){
                 whoTakesRound.style.display = "flex";
                 winnerX.style.display = "block";
                 winnerO.style.display = "none";
                 p2WinScore++;
-                console.log(p2WinScore)
                 p2Score.innerHTML = p2WinScore;
+                clickCounter = 1;
+                winnerPlayer.innerHTML = "2"
                 
+            }else if(clickCounter > 9){
+                ties.style.display = "block";
+                whoTakesRound.style.display = "flex";
+                winner.style.display = "none"
+                winnerInfo.style.display = "none";
+                clickCounter = 1;
+                tiesWinScore++;
+                tiesScore.innerHTML = tiesWinScore;
             }
             
         }else if(!box.clicked && clickCounter % 2 === 0){
             box.querySelector(".full-O").style.display = "block";
             Oturn.style.display = "none";
-            Xturn.style.display = "block"
-            clickCounter++;
+            Xturn.style.display = "block";      
             box.querySelector(".part-O").style.display = "none";
+            clickCounter++;
             if((gameBoxes[0].querySelector(".full-O").style.display == "block" && gameBoxes[1].querySelector(".full-O").style.display == "block" && gameBoxes[2].querySelector(".full-O").style.display == "block") ||
             (gameBoxes[0].querySelector(".full-O").style.display == "block" && gameBoxes[3].querySelector(".full-O").style.display == "block" && gameBoxes[6].querySelector(".full-O").style.display == "block") ||
             (gameBoxes[0].querySelector(".full-O").style.display == "block" && gameBoxes[4].querySelector(".full-O").style.display == "block" && gameBoxes[8].querySelector(".full-O").style.display == "block") ||
-            (gameBoxes[1].querySelector(".full-O").style.display == "block" && gameBoxes[4].querySelector(".full-O").style.display == "block" && gameBoxes[7].querySelector(".full-O").style.display == "block")){
+            (gameBoxes[1].querySelector(".full-O").style.display == "block" && gameBoxes[4].querySelector(".full-O").style.display == "block" && gameBoxes[7].querySelector(".full-O").style.display == "block") ||
+            (gameBoxes[2].querySelector(".full-O").style.display == "block" && gameBoxes[4].querySelector(".full-O").style.display == "block" && gameBoxes[6].querySelector(".full-O").style.display == "block") ||
+            (gameBoxes[2].querySelector(".full-O").style.display == "block" && gameBoxes[5].querySelector(".full-O").style.display == "block" && gameBoxes[8].querySelector(".full-O").style.display == "block") ||
+            (gameBoxes[3].querySelector(".full-O").style.display == "block" && gameBoxes[4].querySelector(".full-O").style.display == "block" && gameBoxes[5].querySelector(".full-O").style.display == "block") ||
+            (gameBoxes[6].querySelector(".full-O").style.display == "block" && gameBoxes[7].querySelector(".full-O").style.display == "block" && gameBoxes[8].querySelector(".full-O").style.display == "block")){
                 whoTakesRound.style.display = "flex";
                 winnerO.style.display = "block";
                 winnerX.style.display = "none";
                 p1WinScore++;
                 p1Score.innerHTML = p1WinScore;
+                clickCounter = 1;
+                winnerPlayer.innerHTML = "1"
+                takesTheRound.style.color = "#F2B137"
+            }else if(clickCounter > 9){
+                whoTakesRound.style.display = "flex";
+                ties.style.display = "block";
+                winner.style.display = "none";
+                winnerInfo.style.display = "none"
+                clickCounter = 1;
+                tiesWinScore++;
+                tiesScore.innerHTML = tiesWinScore;
+               
             }
         }else if(box.clicked){
             box.setAttribute("disabled", true)
